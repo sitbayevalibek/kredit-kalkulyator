b404a472bf0e194176bba7c47324b15f9cffbe09
Alibek <132186570+sitbayevalibek@users.noreply.github.com>
2023-07-10 12:40:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def credit_decision(yosh, jins, ish, uy, oylik_miqdori, oy, plastik_karta, p2p_xarajatlari, foiz):
+def credit_decision(yosh, jins, ish, uy, oylik_miqdori, oylik_xarajatlari, oy, plastik_karta, p2p_xarajatlari, foiz):
     # Yosh
     narx = 0
     if 18 <= yosh <= 30:
@@ -44,33 +44,33 @@ def credit_decision(yosh, jins, ish, uy, oylik_miqdori, oy, plastik_karta, p2p_x
 
 
     # Kredit miqdorini hisoblash
-    kredit_miqdori = (oylik_miqdori * oy) / (foiz / 100) - narx
+    kredit_miqdori = ((oylik_miqdori-oylik_xarajatlari) * oy) / (foiz / 100) - narx
     return kredit_miqdori
 
 
     # Uyining mavjudligiga qarab kreditni baholash
     if uy == "Bor":
         narx = 10_000_000
-        kredit_miqdori = (oylik_miqdori*oy)/(foiz/100) + narx
+        kredit_miqdori = ((oylik_miqdori-oylik_xarajatlari)*oy)/(foiz/100) + narx
     if uy == "Yo'q":
         narx = 25_000_000
-        kredit_miqdori = (oylik_miqdori*oy)/(foiz/100) - narx
+        kredit_miqdori = ((oylik_miqdori-oylik_xarajatlari)*oy)/(foiz/100) - narx
         return kredit_miqdori
     # Plastik karta mavjudligiga qarab kreditni baholash
     if plastik_karta == "Yo'q":
         narx = 5000_000
-        kredit_miqdori = (oylik_miqdori*oy)/(foiz/100) - narx
+        kredit_miqdori = ((oylik_miqdori-oylik_xarajatlari)*oy)/(foiz/100) - narx
     if plastik_karta == "Bor":
         narx = 5000_000
-        kredit_miqdori = (oylik_miqdori*oy)/(foiz/100) + narx
+        kredit_miqdori = ((oylik_miqdori-oylik_xarajatlari)*oy)/(foiz/100) + narx
         return kredit_miqdori
     # P2P xarajatlari bo'yicha kreditni baholash
     if p2p_xarajatlari >= 100_000:
         narx = 1_000_000
-        kredit_miqdori = (oylik_miqdori*oy)/(foiz/100) + narx
+        kredit_miqdori = ((oylik_miqdori-oylik_xarajatlari)*oy)/(foiz/100) + narx
     if p2p_xarajatlari >=500_000:
         narx = 5_000_000
-        kredit_miqdori = (oylik_miqdori*oy)/(foiz/100) + narx
+        kredit_miqdori = ((oylik_miqdori-oylik_xarajatlari)*oy)/(foiz/100) + narx
         return kredit_miqdori
 def main():
     # Streamlit interfeysini yaratish
@@ -82,6 +82,7 @@ def main():
     ish = st.radio("Ish joyingiz", ["Bor", "Yo'q"])
     uy = st.radio("Uyingiz", ["Bor", "Yo'q"])
     oylik_miqdori = st.slider("Oylik daromadingiz", 1000_000, 20_000_000, step=1000_000)
+    oylik_xarajatlari = st.slider("Oylik xarajatlari (%)", 10, 80, step=5)
     # kredit_miqdori = st.slider("Kredit miqdori", 10_000_000, 100_000_000,  step=10_000_000)
     foiz = st.radio("Yillik foiz stavkasi", [20, 25])
     oy = st.slider("Kredit muddati (oylar)", 12, 36, step=12)
@@ -91,7 +92,7 @@ def main():
     # Kreditni baholash funktsiyasini chaqirish
     # natija = int(credit_decision(yosh, jins, ish, uy, oylik_miqdori,  oy, plastik_karta, p2p_xarajatlari, foiz))
     if st.button('Submit'):
-        natija = credit_decision(yosh, jins, ish, uy, oylik_miqdori,  oy, plastik_karta, p2p_xarajatlari, foiz)
+        natija = credit_decision(yosh, jins, ish, uy, oylik_miqdori,  oy, plastik_karta, oylik_xarajatlari, p2p_xarajatlari, foiz)
         st.write("Berilishi mumkin bo'lgan kredit:")
         if natija <= 0:
            st.error("Kredit berilmaydi")
